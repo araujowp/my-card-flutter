c8bc745f5987255dbd08ad33f743b522041554eb
Wagner.Araujo <araujowp@gmail>
2021-03-30 07:30:49 -0300

    desafio15

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,60 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MeuApp(),
+  );
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Container(),
+        backgroundColor: Colors.lightBlue,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  branco(), branco()
+                ],
+              ),
+              desafio15(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  branco(), branco()
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+Widget branco() {
+  return Container(
+    color: Colors.white,
+    height: 100,
+    width: 100,
+  );
+}
+Widget desafio15() {
+  return Container(
+    color: Colors.black,
+    height: 150,
+    width: 200,
+    child: Center(
+      child: Text('Desafio aula 15',
+      style: TextStyle(fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.white)  ,),
     ),
   );
 }
+
+
