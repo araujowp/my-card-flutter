59a542d73ced08a3944845aae53304ad9fd9b52a
Wagner.Araujo <araujowp@gmail>
2021-04-01 13:10:47 -0300

    final add icones e fontes

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,64 @@ class MeuApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.deepOrange,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  branco(), branco()
-                ],
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/wagnermenor.jpg'),
               ),
-              desafio15(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  branco(), branco()
-                ],
+              Text(
+                'Wagner Araujo',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'DESENVOLVEDOR JAVA',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepOrange.shade100,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(height: 20,width: 100,
+                child: Divider(
+                  color: Colors.deepOrange.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.deepOrange,
+                  ),
+                   title: Text(
+                      '55 11 0000 0000',
+                      style: TextStyle(color: Colors.deepOrange),
+                    ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.deepOrange,
+                  ),
+                title: Text(
+                  'araujowp@gmail.com',
+                  style: TextStyle(color: Colors.deepOrange),
+                ),
+                ),
               ),
             ],
           ),
@@ -44,18 +85,18 @@ Widget branco() {
     width: 100,
   );
 }
+
 Widget desafio15() {
   return Container(
     color: Colors.black,
     height: 150,
     width: 200,
     child: Center(
-      child: Text('Desafio aula 15',
-      style: TextStyle(fontSize: 20,
-      fontWeight: FontWeight.bold,
-      color: Colors.white)  ,),
+      child: Text(
+        'Desafio aula 15',
+        style: TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
     ),
   );
 }
-
-
